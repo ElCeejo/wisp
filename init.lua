1baec42a924bf0c96f332bd121aeea7f03ea5efa
ElCeejo <40281901+ElCeejo@users.noreply.github.com>
2021-07-28 12:07:44 -0700

    Improved Performance, Added Spawning

--- init.lua
+++ init.lua
@@ -40,38 +40,26 @@ local abs = math.abs
 local function interp(a, b, w) return a + (b - a) * w end
 
 local function wisp_visual(pos, color)
-    minetest.add_particlespawner({
-        amount = 3,
-        time = 0.1,
-        minpos = vector.add(pos, -0.1),
-        maxpos = vector.add(pos, 0.1),
-        minvel = {
-            x = -0.5,
-            y = 0.5,
-            z = -0.5
-        },
-        maxvel = {
-            x = 0.5,
-            y = 1,
-            z = 0.5
-        },
-        minacc = {
+    local random_vel = {
+        x = random(-1, 1),
+        y = -1,
+        z = random(-1, 1)
+    }
+    local random_offset = {
+        x = pos.x + (random(-1, 1) * 0.1),
+        y = pos.y + (random(-1, 1) * 0.1),
+        z = pos.z + (random(-1, 1) * 0.1)
+    }
+    minetest.add_particle({
+        pos = random_offset,
+        velocity = random_vel,
+        acceleration = {
             x = 0,
-            y = 2,
-            z = 0
+            y = random(6, 8),
+            z = 0,
         },
-        maxacc = {
-            x = 0,
-            y = 4,
-            z = 0
-        },
-        minexptime = 0.1,
-        maxexptime = 0.1,
-        minsize = 3,
-        maxsize = 5,
-        collisiondetection = false,
-        vertical = false,
-        use_texture_alpha = true,
+        expirationtime = 0.1,
+        size = random(3, 5),
         texture = "wisp_".. color .. ".png",
         glow = 6
     })
@@ -79,7 +67,7 @@ end
 
 local function wisped_effect(pos, color)
     minetest.add_particlespawner({
-        amount = 6,
+        amount = 2,
         time = 0.1,
         minpos = vector.subtract(pos, 0.5),
         maxpos = vector.add(pos, 0.5),
@@ -125,13 +113,15 @@ local wisp_colors = {
 
 local function create_wisp(pos, color)
     local wisp_color = color
-    if string.len(color) == 0 then
+    if not color
+    or string.len(color) == 0 then
         wisp_color = wisp_colors[random(3)]
     end
     local wisp_d = {
         pos = pos,
         new_pos = pos,
-        visual_dtime = 0,
+        self_dtime = 0,
+        lifetime = 0,
         color = wisp_color
     }
     table.insert(wisp.wisps, wisp_d)
@@ -147,58 +137,77 @@ minetest.register_chatcommand("add_wisp", {
 	end,
 })
 
+minetest.register_chatcommand("clear_wisps", {
+	params = "<color>",
+	description = "Clears Wisps",
+    privs = {server = true},
+	func = function(name, color)
+        wisp.wisps = {}
+	end,
+})
+
+
 minetest.register_globalstep(function(dtime)
     for i = 1, #wisp.wisps do
         local c_wisp = wisp.wisps[i]
         if not c_wisp then return end
-        local pos = wisp.wisps[i].pos
-        local color = wisp.wisps[i].color
-        if not c_wisp.move_time then
-            c_wisp.move_time = 0
-        end
-        c_wisp.move_time = c_wisp.move_time + dtime
-        if c_wisp.move_time > 2 then
-            if random(3) > 1 then
-                local new_pos = vector.add(pos, random(-3, 3))
-                if minetest.get_node(new_pos).name == "air" then
-                    c_wisp.new_pos = new_pos
-                end
-            else
-                c_wisp.move_time = 0
-            end
-        end
-        if vector.distance(pos, c_wisp.new_pos) > 0.2 then
-            c_wisp.pos = {
-                x = interp(pos.x, c_wisp.new_pos.x, 0.05),
-                y = interp(pos.y, c_wisp.new_pos.y, 0.05),
-                z = interp(pos.z, c_wisp.new_pos.z, 0.05)
-            }
-        end
+        local pos = c_wisp.pos
+        local color = c_wisp.color
+        local lifetime = c_wisp.lifetime or 0
+        local self_dtime = c_wisp.self_dtime or 0
+        local move_time = c_wisp.move_time or 0
+        lifetime = lifetime + dtime
         wisp_visual(pos, color)
-        local objects = minetest.get_objects_inside_radius(pos, 6)
-        for n = 1, #objects do
-            local obj = objects[n]
-            if obj
-            and obj:is_player() then
-                local pos = obj:get_pos()
-                pos.y = pos.y + 1.4
-                local dir = vector.direction(pos, c_wisp.pos)
-                local flee_dir = {
-                    x = dir.x,
-                    y = random(-1, 1),
-                    z = dir.z
+        c_wisp.self_dtime = self_dtime + dtime
+        if self_dtime > 0.1 then
+            -- Movement
+            move_time = move_time + 0.1
+            if move_time > 2 then
+                if random(3) > 1 then
+                    local new_pos = vector.add(pos, random(-3, 3))
+                    if minetest.get_node(new_pos).name == "air" then
+                        c_wisp.new_pos = new_pos
+                    end
+                else
+                    move_time = 0
+                end
+            end
+            if vector.distance(pos, c_wisp.new_pos) > 0.2 then
+                c_wisp.pos = {
+                    x = interp(pos.x, c_wisp.new_pos.x, 0.05),
+                    y = interp(pos.y, c_wisp.new_pos.y, 0.05),
+                    z = interp(pos.z, c_wisp.new_pos.z, 0.05)
                 }
-                c_wisp.new_pos = vector.add(c_wisp.pos, flee_dir)
-                if obj:get_player_control().LMB
-                and obj:get_wielded_item():get_name() == "wisp:jar" then
-
-                    local look_dir = obj:get_look_dir()
-                    if math.abs(vector.length(vector.subtract(dir, look_dir))) < 0.1 then
-                        obj:set_wielded_item("wisp:jar_" .. c_wisp.color)
-                        table.remove(wisp.wisps, i)
+            end
+            -- Left-click detection
+            local objects = minetest.get_objects_inside_radius(pos, 6)
+            for n = 1, #objects do
+                local obj = objects[n]
+                if obj
+                and obj:is_player() then
+                    local p_pos = obj:get_pos()
+                    p_pos.y = p_pos.y + 1.4
+                    local dir = vector.direction(p_pos, pos)
+                    local flee_dir = {
+                        x = dir.x,
+                        y = random(-1, 1),
+                        z = dir.z
+                    }
+                    c_wisp.new_pos = vector.add(pos, flee_dir)
+                    if obj:get_player_control().LMB
+                    and obj:get_wielded_item():get_name() == "wisp:jar" then
+                        local look_dir = obj:get_look_dir()
+                        if math.abs(vector.length(vector.subtract(dir, look_dir))) < 0.1 then
+                            obj:set_wielded_item("wisp:jar_" .. color)
+                            table.remove(wisp.wisps, i)
+                        end
                     end
                 end
             end
+            self_dtime = 0
+        end
+        if lifetime > 60 then
+            table.remove(wisp.wisps, i)
         end
     end
     for i = 1, #wisp.jars do
@@ -228,7 +237,7 @@ minetest.register_globalstep(function(dtime)
             end
             if color == "red"
             and random(64) < 2 then
-                local nodes = minetest.find_nodes_in_area_under_air(vector.subtract(pos, 5), vector.add(pos, 5), "group:flammable")
+                local nodes = minetest.find_nodes_in_area_under_air(vector.subtract(pos, 5), vector.add(pos, 5), {"group:grass", "group:plant", "group:flora"})
                 if #nodes > 0 then
                     for n = 1, #nodes do
                         minetest.remove_node(nodes[n])
@@ -241,6 +250,17 @@ minetest.register_globalstep(function(dtime)
         end
     end
 end)
+
+minetest.register_abm({
+    label = "wisp:spawning",
+    nodenames = {"group:tree"},
+    neighbors = {"air"},
+    interval = 120,
+    chance = 512,
+    action = function(pos)
+        create_wisp(vector.add(pos, random(-3, 3)))
+    end
+})
 
 -----------
 -- Nodes --
@@ -355,10 +375,10 @@ and minetest.get_modpath("nc_tree") then
     nodecore.register_craft({
 		label = "assemble jar",
 		normal = {y = 1},
-		indexkeys = {"nc_optics:glass", "nc_tree:stick"},
+		indexkeys = {"nc_optics:shelf", "nc_tree:stick"},
 		nodes = {
 			{match = "nc_tree:stick", replace = "air"},
-			{y = -1, match = "nc_optics:glass", replace = "wisp:jar"}
+			{y = -1, match = "nc_optics:shelf", replace = "wisp:jar"}
 		}
 	})
 else
